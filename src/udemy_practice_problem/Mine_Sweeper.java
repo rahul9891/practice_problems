cd0b0a86077b21b007be252afc0a651407072278
Rahul Singh <rks.aug13@gmail.com>
2022-04-15 17:56:48 +0100

    few more problems added

---
+++ src/udemy_practice_problem/Mine_Sweeper.java
@@ -0,0 +1,14 @@
+package udemy_practice_problem;
+
+public class Mine_Sweeper {
+    
+    public static void main(String[] args) {
+        
+    }
+
+    static int[][] mine_sweeper(int bombs[][], int rows, int cols){
+        int[][] result = new int[rows][cols];
+
+        return result;
+    }
+}

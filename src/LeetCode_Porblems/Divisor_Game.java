8d0775718cc2dda48a787b7d2eed18b2ac1a9c00
Rahul Singh <rks.aug13@gmail.com>
2022-05-02 19:38:44 +0100

    problems from eclipse

---
+++ src/LeetCode_Porblems/Divisor_Game.java
@@ -0,0 +1,35 @@
+package LeetCode_Porblems;
+
+
+/**
+ * LeetCode 1025
+ * */
+public class Divisor_Game {
+    public static boolean divisorGame(int n) {
+        int count=0;
+        while(n > 1) {
+            
+            n = n - maxFactor(n);
+            count++;
+        }
+        
+        return count%2 != 0;
+        
+    }
+    
+    static int maxFactor(int n) {
+        
+        int max = 0;
+        
+        for(int i=1;i<n;i++) {
+            if(n%i== 0) {
+                max = Math.max(max, i);
+            }
+        }
+        return max;
+    }
+    
+    public static void main(String[] args) {
+		System.out.println(divisorGame(3));
+	}
+}
